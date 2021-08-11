a4960bda2cf08fcfb43984006fb2382d5f339b0b
Elisa Shapiro <elisashapiro@gmail.com>
2021-08-10 22:17:50 -0400

    commit tree 8f00d194c863a432f81470e98667c5261d77b3d4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :side_a, :side_b, :side_c
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  def kind
+    validate_triangle
+    if side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def sides_positive
+    [side_a, side_b, side_c].all?(&:positive?)
+  end
+  def triangle_inequality_theorem
+    side_a + side_b > side_c && side_a + side_c > side_b &&side_b + side_c > side_a 
+  end
+  def validate_triangle
+    raise TriangleError unless sides_positive && triangle_inequality_theorem
+  end
+  class TriangleError < StandardError
+  end
 end
